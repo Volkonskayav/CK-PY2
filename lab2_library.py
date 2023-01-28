00627d2600098ca8466619ca8c7c5761fc4e9cb1
Volkonskayav <115416971+Volkonskayav@users.noreply.github.com>
2023-01-28 14:01:27 +0300

    Lab2_fix

--- lab2_library.py
+++ lab2_library.py
@@ -25,45 +25,6 @@ class Book:
         return f'Book(id_={self.id_}, name={self.name!r}, pages={self.pages})'
 
 
-if __name__ == '__main__':
-    # инициализируем список книг
-    list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
-    ]
-    for book in list_books:
-        print(book)  # проверяем метод __str__
-
-    print(list_books)  # проверяем метод __repr__
- 59
-lab2/lab2_library.py
-@@ -0,0 +1,59 @@
-BOOKS_DATABASE = [
-    {
-        "id": 1,
-        "name": "test_name_1",
-        "pages": 200,
-    },
-    {
-        "id": 2,
-        "name": "test_name_2",
-        "pages": 400,
-    }
-]
-
-
-class Book:
-    def __init__(self, id_: int, name: str, pages: int):
-        self.id_ = id_
-        self.name = name
-        self.pages = pages
-
-    def __str__(self) -> str:
-        return f'Книга "{self.name}"'
-
-    def __repr__(self):
-        return f'Book(id_={self.id_}, name={self.name!r}, pages={self.pages})'
-
-
 class Library:
     def __init__(self, books=None):
         if books is None:
